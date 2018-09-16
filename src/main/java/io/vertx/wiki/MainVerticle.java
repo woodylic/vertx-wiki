496814dab091b3e16bf89f1c0c7fd5360e933c4b
Woody Lee <woody.lee@ef.com>
2018-09-16 22:49:52 +0800

    Re-write the start method.

--- src/main/java/io/vertx/wiki/MainVerticle.java
+++ src/main/java/io/vertx/wiki/MainVerticle.java
@@ -1,14 +1,29 @@
 package io.vertx.wiki;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.jdbc.JDBCClient;
 
 public class MainVerticle extends AbstractVerticle {
 
+
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
+  public void start(Future<Void> startFuture) throws Exception {
+    Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
+    steps.setHandler(startFuture.completer());
   }
 
+  private Future<Void> prepareDatabase() {
+    Future<Void> future = Future.future();
+    //TODO
+    return future;
+  }
+
+  private Future<Void> startHttpServer() {
+    Future<Void> future = Future.future();
+    //TODO
+    return future;
+  }
 }
