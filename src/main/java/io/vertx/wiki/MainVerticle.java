8245c39ad10378a19f68ff238aef26030d99a588
Woody Lee <woody.lee@ef.com>
2018-09-16 23:07:44 +0800

    complete prepareDatabase method

--- src/main/java/io/vertx/wiki/MainVerticle.java
+++ src/main/java/io/vertx/wiki/MainVerticle.java
@@ -2,12 +2,23 @@ package io.vertx.wiki;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
+  private static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+  private static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
+  private static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
+  private static final String SQL_ALL_PAGES = "select Name from Pages";
+  private static final String SQL_DELETE_PAGE = "delete from Pages where Id = ?";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+  private JDBCClient dbClient;
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
@@ -17,7 +28,35 @@ public class MainVerticle extends AbstractVerticle {
 
   private Future<Void> prepareDatabase() {
     Future<Void> future = Future.future();
-    //TODO
+
+    // createShared creates a shared connection to be shared
+    // among verticles know to the vertx instance.
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+      .put("url", "jdbc:hsqldb:file:db/wiki")
+      .put("driver_class", "org.hsqldb.jdbcDriver")
+      .put("max_pool_size", 30));
+
+    // getConnection returns AsyncResult<SQLConnection>
+    dbClient.getConnection(ar -> {
+      if(ar.failed()) {
+        LOGGER.error("Could not open a database connection", ar.cause());
+        future.fail(ar.cause());
+      } else {
+        SQLConnection connection = ar.result();
+        connection.execute(SQL_CREATE_PAGES_TABLE, create -> {
+          // release the connection back to connection pool,
+          // no matter the create table operation succeeded or not.
+          connection.close();
+          if(create.failed()) {
+            LOGGER.error("Database preparation error", create.cause());
+            future.fail(create.cause());
+          } else {
+            future.complete();
+          }
+        });
+      }
+    });
+
     return future;
   }
 
